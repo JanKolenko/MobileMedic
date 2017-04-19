5697fa0077441af395f6511b624f0d5859057549
Jan Kolenko <jan.kolenko17@gmail.com>
2017-04-19 15:02:08 +0200

    Added MapViewController

--- MobileMedic/ViewController.swift
+++ MobileMedic/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello World!")
+        let mapViewController = MapViewController()
+        print(MapViewController)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
